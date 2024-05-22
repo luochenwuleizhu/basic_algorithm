1bba89a23fa6bbb360454445b729ce30c575dc20
percy <percy@yaocheng.cn>
2024-05-22 14:26:57 +0800

    update algorithm

--- test_algorithm/quick_sort.py
+++ test_algorithm/quick_sort.py
@@ -12,7 +12,8 @@ def quick_sort(arr):
 
     return quick_sort(left) + middle + quick_sort(right)
 
-# 示例用法
+
+# 测试数据
 arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
 if __name__ == "__main__":
     sorted_arr = quick_sort(arr)
